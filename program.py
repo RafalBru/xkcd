ede94342b2ed7c3d4cdc5a83b1e74a1ee6115517
RafalBru <93768384+RafalBru@users.noreply.github.com>
2024-07-07 23:10:50 +0200

    Add files via upload

--- program.py
+++ program.py
@@ -17,18 +17,42 @@ for number in numbers:
     image = download_images(number)
     images.append(image)
 
-with open('test.html','r') as file:
-    html_content = file.read()
+content = """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comics</title>
+    <style>
+        body {
+            text-align: center;
+        }
+        img {
+            border-color: blue;
+            border-width: 3px;
+        }
+    </style>
+</head>
+<body>
 
-soup = BeautifulSoup(html_content,'html.parser')
+</body>
+</html>
+"""
+#with open('test.html','r') as file:
+ #   html_content = file.read()
+
+soup = BeautifulSoup(content,'html.parser')
 
 comics_list = soup.find('body')
 for image_url in images:
     img = soup.new_tag('img',src=image_url)
-    img['width'] = "100%"
+    img['width'] = "500px"
+    #img['height'] = "200px"
+    
     hr = soup.new_tag('hr')
     comics_list.append(img)
     comics_list.append(hr)
 
-with open('index_modified.html', 'w') as file:
+with open('index.html', 'w') as file:
     file.write(soup.prettify())

09434e09a424473aca123c66db4cc5f00f5a3073
RafalBru <93768384+RafalBru@users.noreply.github.com>
2024-07-07 18:09:45 +0200

    Add files via upload

--- program.py
+++ program.py
@@ -1,11 +1,9 @@
-# pobieranie liczb z pliku
-# łączenie z serwerem
-# # z pomocą api pobrać obrazki bądź linki
-# generacja pliku html (uzyc bootstrap)
 import requests
+from bs4 import BeautifulSoup
 
 with open('./nr.txt','r') as file:
     numbers = file.read().split('\n')
+
 images = []
 print(numbers)
 
@@ -13,13 +11,24 @@ def download_images(number):
     url = "https://xkcd.com/" + number + "/info.0.json"
     response = requests.get(url)
     data = response.json()
+    return data['img']
 
-    url_image = data['img']
-    image = requests.get(url_image)
-    return image
+for number in numbers:
+    image = download_images(number)
+    images.append(image)
 
-# for number in numbers:
-#     image = download_images(number)
-#     images.append(image)
+with open('test.html','r') as file:
+    html_content = file.read()
 
-#with open('index.html','w') as index:
+soup = BeautifulSoup(html_content,'html.parser')
+
+comics_list = soup.find('body')
+for image_url in images:
+    img = soup.new_tag('img',src=image_url)
+    img['width'] = "100%"
+    hr = soup.new_tag('hr')
+    comics_list.append(img)
+    comics_list.append(hr)
+
+with open('index_modified.html', 'w') as file:
+    file.write(soup.prettify())

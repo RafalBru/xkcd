42d319c057664d2d7e2d3d6580616ad8f2ddb7e7
RafalBru <93768384+RafalBru@users.noreply.github.com>
2024-07-08 20:19:54 +0200

    gotowa strona

--- program.py
+++ program.py
@@ -5,54 +5,52 @@ with open('./nr.txt','r') as file:
     numbers = file.read().split('\n')
 
 images = []
+titles = []
 print(numbers)
 
 def download_images(number):
     url = "https://xkcd.com/" + number + "/info.0.json"
     response = requests.get(url)
     data = response.json()
-    return data['img']
+    return data['img'], data['title']
 
 for number in numbers:
-    image = download_images(number)
+    image, title = download_images(number)
     images.append(image)
+    titles.append(title)
 
-content = """
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Comics</title>
-    <style>
-        body {
-            text-align: center;
-        }
-        img {
-            border-color: blue;
-            border-width: 3px;
-        }
-    </style>
-</head>
-<body>
-
-</body>
-</html>
-"""
-#with open('test.html','r') as file:
- #   html_content = file.read()
+with open("./komiksy-kopia.html", 'r') as file:
+    content = file.read()
 
 soup = BeautifulSoup(content,'html.parser')
 
-comics_list = soup.find('body')
-for image_url in images:
-    img = soup.new_tag('img',src=image_url)
-    img['width'] = "500px"
-    #img['height'] = "200px"
-    
-    hr = soup.new_tag('hr')
-    comics_list.append(img)
-    comics_list.append(hr)
+comics_list = soup.find('section')
+for number, image_url, image_title in zip(numbers,images,titles):
+    article = soup.new_tag('article',class_="my-3")
+    div = soup.new_tag('div',class_="bd-heading sticky-xl-top align-self-start mt-5 mb-3 mt-xl-0 mb-xl-2")
+    title = soup.new_tag('h3')
+    title.string = number + ' : ' + image_title
+    div2 = soup.new_tag('div')
 
-with open('index.html', 'w') as file:
+    snippet = soup.new_tag('div',class_="bd-example-snippet bd-code-snippet")
+    example = soup.new_tag('div',class_="bd-example m-0 border-0")
+
+    img = soup.new_tag('img',src=image_url)
+    img['width'] = "100%"
+    img['height'] = "30%"
+
+    hr = soup.new_tag('hr')
+
+    example.append(img)
+    example.append(hr)
+    snippet.append(example)
+    div2.append(snippet)
+    div.append(title)
+    article.append(div)
+    article.append(div2)
+    comics_list.append(article)
+
+with open('comics.html', 'w') as file:
     file.write(soup.prettify())
+
+print("Strona z wybranymi jest gotowa. Otworz plik comics.html w przegladarce.")
